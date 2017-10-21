822d6f9cb6c09dfc491ed8e77568e32fe12384f6
futuremarc <mas1529@nyu.edu>
2017-10-21 12:37:33 -0400

    adds total spots 50k to contract

--- contracts/LoveLocks.sol
+++ contracts/LoveLocks.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.4.11;
 
 contract LoveLocks{
 
-  LoveLock[] public lovelocks;
+  LoveLock[] loveLocks;
+  uint totalSpots = 50000;
 
 	struct LoveLock{
     bytes8 color;
@@ -17,7 +18,16 @@ contract LoveLocks{
     address id;
 	}
 
-  function addLoveLock(bytes8 _color, bytes32 _personA, bytes32 _personB, bytes32 _message1, bytes32 _message2, bytes32 _message3,bytes32 _message4, uint8 _xPos, uint8 _yPos, address _id) public returns (bool success){
+  modifier isSpotAvail(){
+    bool avail = true;
+    if (loveLocks.length >= totalSpots){
+     isSpotAvail = false;
+    }
+    require(avail);
+    _;
+  }
+
+  function addLoveLock(bytes8 _color, bytes32 _personA, bytes32 _personB, bytes32 _message1, bytes32 _message2, bytes32 _message3,bytes32 _message4, uint8 _xPos, uint8 _yPos) isSpotAvail public returns (bool success) {
     LoveLock memory newLoveLock;
     newLoveLock.color = _color;
     newLoveLock.personA = _personA;
@@ -28,13 +38,13 @@ contract LoveLocks{
     newLoveLock.message4 = _message4;
     newLoveLock.xPos = _xPos;
     newLoveLock.yPos = _yPos;
-    newLoveLock.id = _id;
-    lovelocks.push(newLoveLock);
+    newLoveLock.id = msg.sender;
+    loveLocks.push(newLoveLock);
     return true;
   }
 
   function getLoveLocks() public constant returns (bytes8[], bytes32[], bytes32[], uint8[],uint8[],address[]) {
-    uint length = lovelocks.length;
+    uint length = loveLocks.length;
     bytes8[] memory colors = new bytes8[](length);
     bytes32[] memory personsA = new bytes32[](length);
     bytes32[] memory personsB = new bytes32[](length);
@@ -42,10 +52,10 @@ contract LoveLocks{
     uint8[] memory yPoses = new uint8[](length);
     address[] memory ids = new address[](length);
 
-    for (uint i = 0; i < lovelocks.length; i++){
+    for (uint i = 0; i < loveLocks.length; i++){
 
       LoveLock memory currentLoveLock;
-      currentLoveLock = lovelocks[i];
+      currentLoveLock = loveLocks[i];
 
       colors[i] = currentLoveLock.color;
       personsA[i] = currentLoveLock.personA;
@@ -54,20 +64,21 @@ contract LoveLocks{
       yPoses[i] = currentLoveLock.yPos;
       ids[i] = currentLoveLock.id;
     }
-    return (colors, personsA,personsB,xPoses,yPoses,id);
+    return (colors, personsA,personsB,xPoses,yPoses,ids);
   }
 
   function getLoveLockMsgs() public constant returns ( bytes32[], bytes32[], bytes32[], bytes32[]) {
-    uint length = lovelocks.length;
+    uint length = loveLocks.length;
+
     bytes32[] memory messages1 = new bytes32[](length);
     bytes32[] memory messages2 = new bytes32[](length);
     bytes32[] memory messages3 = new bytes32[](length);
     bytes32[] memory messages4 = new bytes32[](length);
 
-    for (uint i = 0; i < lovelocks.length; i++){
+    for (uint i = 0; i < loveLocks.length; i++){
 
       LoveLock memory currentLoveLock;
-      currentLoveLock = lovelocks[i];
+      currentLoveLock = loveLocks[i];
 
       messages1[i] = currentLoveLock.message1;
       messages2[i] = currentLoveLock.message2;
